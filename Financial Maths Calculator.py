4b7dbc57e19b961fb2a52459c39a4c561a93d02c
19JIvan <19jivan@mlmc.vic.edu.au>
2017-12-08 11:40:30 +1100

    adding them so i can use them on my desktop

---
+++ Financial Maths Calculator.py
@@ -0,0 +1,16 @@
+import math;
+
+I = 'yes'
+
+print()
+print()
+print()
+
+
+while I != 'no':
+    Type = input("Do you want to apply a percentage 'Discount', 'Markup' or find the percentage of a 'Profit/Loss' if not hit enter.")
+    if Type != 'discount' or Type != 'Discount' or Type != 'markup' or Type != 'Markup' or Type != '':
+        print('Please type the one you want')
+    if Type = 'discount' or Type = 'Discount' or Type = 'markup' or Type = 'Markup' or Type = '':
+        I = 'no'
+        break
